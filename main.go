1529a7f61d1227390f5e440418d839d2c63eda3b
minpeter <kali2005611@gmail.com>
2021-12-31 20:04:13 +0900

    페이지 전체를 불러오도록 수정

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -17,19 +20,44 @@ type extractedJob struct {
 	summary string
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+var baseURL string = "https://kr.indeed.com/jobs?q=linux&limit=50"
 
 func main() {
 	var jobs []extractedJob
-	totalPages := getPages()    //페이지 전체를 불러오도록 수정해야됨
+	totalPages := getPages()
 
 	for i:=0; i<totalPages; i++ {
 		extractedJob := getPage(i)
 		jobs = append(jobs, extractedJob...)
 	}
 	
-	fmt.Println(jobs)
+	writeJobs(jobs)
+	fmt.Println("Done, extracted", len(jobs), "jobs")
 }
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+	utf8bom := []byte{0xEF, 0xBB, 0xBF}
+	file.Write(utf8bom)
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"LINK", "Title", "Location", "Salary", "Summary"}
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+}
+
+func CleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
 
 func getPage(page int) []extractedJob {
 	var jobs []extractedJob
@@ -58,10 +86,10 @@ func extractJob(card *goquery.Selection) extractedJob {
 	id, tf := card.Attr("data-jk")
 	
 	if tf {
-		title := card.Find(".jobTitle>span").Text()
-		location := card.Find(".companyLocation").Text()
-		salary := card.Find(".salary-snippet").Text()
-		summary := card.Find(".job-snippet").Text()
+		title := CleanString(card.Find(".jobTitle>span").Text())
+		location := CleanString(card.Find(".companyLocation").Text())
+		salary := CleanString(card.Find(".salary-snippet").Text())
+		summary := CleanString(card.Find(".job-snippet").Text())
 
 		return extractedJob{id:id, title:title, location:location, salary:salary, summary:summary}
 	} else {
@@ -82,11 +110,15 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 	
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
-
-	return pages
+	searchCountStr := doc.Find("#searchCountPages").Text()
+	searchCountStr = strings.Replace(searchCountStr, ",", "", -1)
+	searchCountStr = strings.Replace(searchCountStr, "1페이지 결과 ", "", -1)
+	searchCountStr = strings.Replace(searchCountStr, "건", "", -1)
+	pages, err = strconv.Atoi(CleanString(searchCountStr))
+	checkErr(err)
+	fmt.Println("find ", pages / 50, "pages")
+	
+	return pages / 50
 }
 
 func checkErr(err error) {
